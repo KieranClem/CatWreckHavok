ff4fe9ee70d45515794f3389abd52dcca74b0fcd
KieranClem <76912798+KieranClem@users.noreply.github.com>
2022-06-24 11:05:04 +0100

    Added furniture manager and furniture script
    made the furniture manager to track the amount of furniture on the level, all the ruined furniture and also made a furniture script that should update the furniture manager if collided with the player, although not been able to test that. Also set the ability for the furniture to be reset if needed.

---
+++ Assets/Scripts/Furniture.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Furniture : MonoBehaviour
+{
+    //Tracks if the player has knocked it down
+    [HideInInspector] public bool bKnockedDown = false;
+    //Furniture manager is stored here for access, this variable is set in the furniture manager script in the awake method
+    [HideInInspector] public FurnitureManager furnitureManager;
+    //Stores furnitures default position
+    public Vector3 fDefaultPosition;
+
+    private void Awake()
+    {
+        fDefaultPosition = this.transform.position;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //if the furniture collides with the player, the furniture will be added to the ruined furniture and be knocked down
+        if(collision.tag == "Player" && !bKnockedDown)
+        {
+            furnitureManager.addRuinedFurniture(this);
+            bKnockedDown = true;
+        }
+    }
+}
---
+++ Assets/Scripts/FurnitureManager.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FurnitureManager : MonoBehaviour
+{
+    //Lists to store the furniture and ruined furniture in for tracking purposes
+    private List<Furniture> sceneFurniture = new List<Furniture>();
+    private List<Furniture> ruinedFurniture = new List<Furniture>();
+    //Number of furntiture needed to finish the level which can be changed in the inspector, default is 1
+    public int iNumOfFurnitureForLevelFinish = 1;
+
+    private void Awake()
+    {
+        //Gets all the gameobjects on the scene with the furniture tag and adds them to the scene furniture list
+        foreach (GameObject furn in GameObject.FindGameObjectsWithTag("Furniture"))
+        {
+            Furniture furniture = furn.GetComponent<Furniture>();
+            sceneFurniture.Add(furniture);
+            //Stores the furniture manager in the furniture's script for easy access
+            furniture.furnitureManager = this;
+        }
+    }
+
+    //called when the player knocks down some furniture to add to the ruined furniture list
+    public void addRuinedFurniture(Furniture ruinFurniture)
+    {
+        ruinedFurniture.Add(ruinFurniture);
+        //Checks if the player has knocked down enough furniture by counting the amount of game objects in the list
+        if(ruinedFurniture.Count > iNumOfFurnitureForLevelFinish)
+        {
+            //Add code to allow for exit to open here
+        }
+    }
+
+    //Resets furniture postions should we need to
+    public void ResetFurniturePositions()
+    {
+        foreach(Furniture furniture in ruinedFurniture)
+        {
+            furniture.transform.position = furniture.fDefaultPosition;
+            furniture.bKnockedDown = false;
+        }
+        ruinedFurniture.Clear();
+    }
+}
