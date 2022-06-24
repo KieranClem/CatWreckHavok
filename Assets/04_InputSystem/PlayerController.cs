36f27a518e2290ef1d36817e3fe9b2a3c7b5184b
KieranClem <76912798+KieranClem@users.noreply.github.com>
2022-06-24 10:13:51 +0100

    KC Movement basics added
    Jump and left/right movement is added and working as intended.

--- Assets/04_InputSystem/PlayerController.cs
+++ Assets/04_InputSystem/PlayerController.cs
@@ -1,9 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    private Rigidbody2D rigidbody2D;
+    private PlayerInput inputActions;
+    
+    private PlayerInputActions playerInputActions;
+    
+
+    public float fJumpForce = 5f;
+    public float fSpeed = 1f;
+    
+
+    private void Awake()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        inputActions = GetComponent<PlayerInput>();
+        playerInputActions = new PlayerInputActions();
+        playerInputActions.Player.Enable();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -11,8 +30,17 @@ public class PlayerController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        Vector2 inputVector = playerInputActions.Player.Movement.ReadValue<Vector2>();
+        rigidbody2D.AddForce(new Vector2(inputVector.x, 0) * fSpeed, ForceMode2D.Impulse);
+    }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if(context.performed)
+        {
+            rigidbody2D.AddForce(Vector2.up * fJumpForce, ForceMode2D.Impulse);
+        }    
     }
 }
